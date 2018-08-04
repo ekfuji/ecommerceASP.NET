2c5384935d08b39e1d1c60fca9688041103a7525
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-08-03 23:43:09 -0300

    Criando (Controller, View Index, Classe Produtos, Context, Migration)

---
+++ ecommerce/ecommerce/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ecommerce.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ecommerce.Models.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ecommerce.Models.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ ecommerce/ecommerce/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ecommerce.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbEcommerce") { }
+
+        //Mapear as classes que vão virar tabela no banco
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
