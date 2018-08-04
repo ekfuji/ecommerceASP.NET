0058e42e49c59e63b3e72d2239791afcd16c8121
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-08-04 00:24:24 -0300

    Implementando (ProdutoController, ProdutoDAO, View Index)

--- ecommerce/ecommerce/Controllers/ProdutoController.cs
+++ ecommerce/ecommerce/Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ecommerce.DAL;
+using ecommerce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,81 @@ namespace ecommerce.Controllers
 {
     public class ProdutoController : Controller
     {
+        //identacao CTRL+K+D
+
+        #region View Index
         // GET: Produto
         public ActionResult Index()
         {
+            ViewBag.Data = DateTime.Now;
+            ViewBag.Produtos = ProdutoDAO.ReturnProdutos();
             return View();
         }
+        #endregion
+
+        #region Pag Cadastrar Produto
+        public ActionResult CadastrarProduto()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Cadastrando Produto
+        [HttpPost]
+        public ActionResult CadastrarProduto(string txtNome, string txtDescricao,
+            string txtPreco, string txtCategoria)
+        {
+            Produto produto = new Produto
+            {
+                Nome = txtNome,
+                Descricao = txtDescricao,
+                Preco = Convert.ToDouble(txtPreco),
+                Categoria = txtCategoria
+            };
+
+            ProdutoDAO.CadastrarProduto(produto);
+
+            return RedirectToAction("Index", "Produto");
+        }
+        #endregion
+
+        #region Remover Produto
+        public ActionResult RemoverProduto(int id)
+        {
+
+            ProdutoDAO.RemoverProduto(id);
+            return RedirectToAction("Index", "Produto");
+            //não esquecer de utilizar o RedirectToAction("Nome da página", "Controller") pois ao não utiliza-lo gerará uma excessão de arquivo não encontrado
+
+        }
+        #endregion
+
+        #region Buscar Produto (AlterarProduto)
+        public ActionResult AlterarProduto(int id)
+        {
+            ViewBag.Produto = ProdutoDAO.BuscarProduto(id);
+            return View();
+        }
+        #endregion
+
+        #region Alterando Produto
+        [HttpPost]
+        public ActionResult AlterarProduto(int txtid, string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
+        {
+            Produto produto = ProdutoDAO.BuscarProduto(txtid);
+            produto.Nome = txtNome;
+            produto.Descricao = txtDescricao;
+            produto.Preco = Convert.ToDouble(txtPreco);
+            produto.Categoria = txtCategoria;
+
+            ProdutoDAO.AlterarProduto(produto);
+
+            // ctx.Entry(produto).State = System.Data.Entity.EntityState.Modified;
+
+
+            return RedirectToAction("Index", "Produto");
+        }
+        #endregion
+
     }
 }
---
+++ ecommerce/ecommerce/DAL/ProdutoDAO.cs
@@ -0,0 +1,56 @@
+﻿using ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ecommerce.DAL
+{
+    public class ProdutoDAO
+    {
+        private static Context ctx = new Context();
+
+        #region Listar Produtos
+        public static List<Produto> ReturnProdutos()
+        {
+            return ctx.Produtos.ToList();
+        }
+        #endregion
+
+        #region Cadastrar Produto
+        public static void CadastrarProduto(Produto produto)
+        {
+            ctx.Produtos.Add(produto);
+            ctx.SaveChanges();
+
+        }
+        #endregion
+
+        #region Remover Produto
+        public static void RemoverProduto(int id)
+        {
+            Produto produto = new Produto();
+            produto = BuscarProduto(id);
+            ctx.Produtos.Remove(produto);
+            ctx.SaveChanges();
+        }
+        #endregion
+
+        #region Buscar Produto
+        public static Produto BuscarProduto(int id)
+        {
+            return ctx.Produtos.Where(x => x.ProdutoId == id).FirstOrDefault();
+        }
+        #endregion
+
+        #region Alterar Produto
+        public static void AlterarProduto(Produto produto)
+        {
+            ctx.Entry(produto).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+        #endregion
+
+    }
+}
