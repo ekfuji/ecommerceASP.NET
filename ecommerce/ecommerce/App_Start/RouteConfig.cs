21291cfd8d96899aafcd00023f17e28e39257255
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-08-04 01:04:05 -0300

    Adicionando Layout com bootstrap

--- ecommerce/ecommerce/App_Start/RouteConfig.cs
+++ ecommerce/ecommerce/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ecommerce
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
